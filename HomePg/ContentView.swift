4b937c80f17dd2567126f726a79e24177618eb7e
Annika <annikaamadi@gmail.com>
2023-07-20 12:52:26 -0700

    changed background color for study-buddy to pink and corner radius to 50

--- HomePg/ContentView.swift
+++ HomePg/ContentView.swift
@@ -9,81 +9,124 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            ScrollView{
-            ZStack {
-                LinearGradient(colors: [Color("barbie"), Color("tan"),Color("ocean"),Color("purp")], startPoint:.top , endPoint: .bottom)
-                .edgesIgnoringSafeArea(.all)
-                // Background color - soft pink
-                //Color(red: 0.96, green: 0.87, blue: 0.89)
+           
+                ZStack {
+                    LinearGradient(colors: [Color("barbie"), Color("tan"),Color("ocean"),Color("purp")], startPoint:.top , endPoint: .bottom)
+                        .edgesIgnoringSafeArea(.all)
+                    // Background color - soft pink
+                    ScrollView{
+                    //Color(red: 0.96, green: 0.87, blue: 0.89)
                     //.edgesIgnoringSafeArea(.all)
-                VStack {
-                    // Page title
-                    Text("Study-Buddy")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color(red: 0.97, green: 0.66, blue: 0.69))
-                        .padding(.top, 20)
-                    // Calendar
-                    CalendarView()
-                        .padding(.top, 30)
-                    // To-Do List
-                    ToDoListView()
-                        .padding(.top, 30)
-                    Spacer()
+                        VStack {
+                            // Page title
+                            Text("Study-Buddy")
+                                .font(.largeTitle)
+                                .fontWeight(.heavy)
+                                .foregroundColor(Color(red: 0.97, green: 0.66, blue: 0.69))
+                                .padding(.top, 20)
+                                .background(.pink)
+                                .cornerRadius(50)
+                        } // Calendar{
+                            RoundedRectangle(cornerRadius: 50)
+                            .foregroundColor(.white)
+                            .frame(height: 200)
+                            .padding(.horizontal)
+                            .padding(.top, 30)
+                        CalendarView()
+                            .padding(.top, 30)
+                        // To-Do List
+                        ToDoListView()
+                            .padding(.top, 30)
+                        Spacer()
+                    }
                 }
+                .navigationBarHidden(true)
             }
-            .navigationBarHidden(true)
         }
     }
-}
-struct CalendarView: View {
-    var body: some View {
-        // Implement your calendar view here
-        // This can be a custom view or a third-party library
-        Text("Calendar")
-            .font(.title)
-            .fontWeight(.light)
-            .foregroundColor(Color(hue: 0.984, saturation: 0.081, brightness: 0.93))
-        DatePicker(
-            "Start Date",
-            selection: $date,
-            in: dateRange,
-            displayedComponents: [.date, .hourAndMinute]
-        )
-    };@State private var date = Date()
-    let dateRange: ClosedRange<Date> = {
-        let calendar = Calendar.current
-        let startComponents = DateComponents(year: 2023, month: 7, day: 1)
-        let endComponents = DateComponents(year: 2023, month: 12, day: 31, hour: 23, minute: 59, second: 59)
-        return calendar.date(from:startComponents)
+    struct CalendarView: View {
+                             
+        
+        let dateRange: ClosedRange<Date> = {
+            let calendar = Calendar.current
+            let startComponents = DateComponents(year: 2023, month: 1, day: 1)
+            let endComponents = DateComponents(year: 2023, month: 12, day: 31, hour: 23, minute: 59, second: 59)
+            return calendar.date(from:startComponents)! ...
+                calendar.date(from:endComponents)!
+               
+        }()
+        @State private var date = Date()
+        var body: some View {
+            // Implement your calendar view here
+            // This can be a custom view or a third-party library
+            Text("Calendar")
+                .font(.title)
+                .fontWeight(.heavy)
+                .foregroundColor(Color(red: 0.97, green: 0.66, blue: 0.69))
+                .padding(.top, 20)
+                        DatePicker(
+                "Start Date",
+                selection: $date,
+                in: dateRange,
+                displayedComponents: [.date, .hourAndMinute]
+               
+            )
+        }
+    
+        
+        
+    }
+        
+    struct ToDoListView: View {
+        var body: some View {
+            // Implement your to-do list view here
+            // This can be a custom view or a list with tasks
+            Text("To-Do List")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color(hue: 0.578, saturation: 0.298, brightness:0.979))
             
-            calendar.date(from:endComponents)!
-    }()
-
-    
-}
-struct ToDoListView: View {
-    var body: some View {
-        // Implement your to-do list view here
-        // This can be a custom view or a list with tasks
-        Text("To-Do List")
-            .font(.title)
-            .fontWeight(.bold)
-            .foregroundColor(Color(hue: 0.578, saturation: 0.298, brightness: 0.979))
-    }
-    
-    struct HomeView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
+        }; struct TaskItemView: View {
+            var taskTitle: String
+            var body: some View {
+                HStack {
+                    Image(systemName: "circle")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(Color(red: 0.97, green: 0.66, blue: 0.69))
+                    Text(taskTitle)
+                        .foregroundColor(.black)
+                    Spacer()
+                    Image(systemName: "checkmark.circle.fill")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(.green)
+                }
+                .padding(.vertical, 4)
+                .background(Color.white)
+                .cornerRadius(10)
+                .shadow(color: .gray, radius: 2, x: 0, y: 2)
+                .padding(.vertical, 4)
+            }
         }
+
+        
+        
+    
+        
+        struct HomeView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
