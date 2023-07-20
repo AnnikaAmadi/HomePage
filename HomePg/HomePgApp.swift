ef4046be0993e5c6f652cae6520fe2c743378d9a
Annika <annikaamadi@gmail.com>
2023-07-20 08:19:13 -0700

    Initial Commit

---
+++ HomePg/HomePgApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomePgApp.swift
+//  HomePg
+//
+//  Created by Scholar on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct HomePgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
